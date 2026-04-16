2207b6624bc38d9eff038882286731a4cff74625
SHAIKADIL522 <your-github-email@gmail.com>
2026-04-16 12:29:16 +0530

    fixed the action.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,13 +2,14 @@ export const FETCH_LOREM_REQUEST = "FETCH_LOREM_REQUEST";
 export const FETCH_LOREM_SUCCESS = "FETCH_LOREM_SUCCESS";
 export const FETCH_LOREM_FAILURE = "FETCH_LOREM_FAILURE";
 
-export const fetchLorem = () => async (dispatch) => {
+export const fetchLorem = () => (dispatch) => {
   dispatch({ type: FETCH_LOREM_REQUEST });
-  try {
-    const response = await fetch("https://api.lorem.com/ipsum");
-    const data = await response.json();
-    dispatch({ type: FETCH_LOREM_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: FETCH_LOREM_FAILURE, payload: error.message });
-  }
+  fetch("https://api.lorem.com/ipsum")
+    .then((response) => response.json())
+    .then((data) => {
+      dispatch({ type: FETCH_LOREM_SUCCESS, payload: data });
+    })
+    .catch((error) => {
+      dispatch({ type: FETCH_LOREM_FAILURE, payload: error.message });
+    });
 };
