260ba4adf0aa2b76512f2b1b3f5e22232197af64
SHAIKADIL522 <your-github-email@gmail.com>
2026-04-19 19:58:54 +0530

    fix: remove setTimeout entirely, always expose window.store, let cy.intercept control timing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { Provider } from "react-redux";
 import App from "./components/App";
 import store from "./store/store";
 
-if (window.Cypress) {
-  window.store = store;
-}
+window.store = store;
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions.js
+++ src/store/actions.js
@@ -5,16 +5,12 @@ export const FETCH_LOREM_FAILURE = "FETCH_LOREM_FAILURE";
 export const fetchLorem = () => (dispatch) => {
   dispatch({ type: FETCH_LOREM_REQUEST });
 
-  const delay = window.Cypress ? 3000 : 0;
-
-  setTimeout(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: FETCH_LOREM_SUCCESS, payload: data });
-      })
-      .catch((err) => {
-        dispatch({ type: FETCH_LOREM_FAILURE, payload: err.message });
-      });
-  }, delay);
+  fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((res) => res.json())
+    .then((data) => {
+      dispatch({ type: FETCH_LOREM_SUCCESS, payload: data });
+    })
+    .catch((err) => {
+      dispatch({ type: FETCH_LOREM_FAILURE, payload: err.message });
+    });
 };
